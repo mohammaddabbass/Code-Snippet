a5d3f85f30e3e04f3e786b03b0d11f602837e1d1
Mohammad Abbas <mohammadabbass0701@gmail.com>
2025-03-23 17:15:04 +0600

    edited and tested jwt authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::group(["prefix" => "v0.1"], function() {
     Route::group(["middleware" => "auth:api"], function() {
